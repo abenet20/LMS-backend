885ea530291dc868eced5cc29b3b81edbbea699a
Abenet Erbelo <abeneterbelo@gmail.com>
2025-12-06 00:45:30 -0800

    //

--- controllers/admin/resource.js
+++ controllers/admin/resource.js
@@ -3,28 +3,31 @@ const verifyToken = require("../../middleware/verifyToken");
 
 const addResource = async (req, res) => {
     try {
-        const { courseId, title, url } = req.body;
-        const type = req.resourceType;
-
-        let fileUrl = url;
-
-        if (type !== "link") {
-            if (!req.files || !req.files.file) {
-                return res.status(400).json({ success: false, message: "File missing" });
-            }
-            fileUrl = req.files.file[0].path;
+      const { courseId, title, url } = req.body;
+      const type = req.resourceType;
+  
+      let fileUrl = url; // default for links
+  
+      if (type !== "link") {
+        if (!req.fileUrl) {
+          return res.status(400).json({ success: false, message: "File missing or upload failed" });
         }
-
-        const resource = await database.query(
-            "INSERT INTO resources (course_id, title, type, url) VALUES (?, ?, ?, ?)",
-            [courseId, title, type, fileUrl]
-        );
-
-        res.status(200).json({ success: true, resource });
+        fileUrl = req.fileUrl;
+      }
+  
+      const resource = await database.query(
+        "INSERT INTO resources (course_id, title, type, url) VALUES (?, ?, ?, ?)",
+        [courseId, title, type, fileUrl]
+      );
+  
+      res.status(200).json({ success: true, resource });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+      console.error(error);
+      res.status(500).json({ success: false, message: error.message });
     }
-};
+  };
+  
+  
 
 
 const deleteResource = async (req, res) => {
--- controllers/data/supabase.js
+++ controllers/data/supabase.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 
 const supabaseUrl = 'https://eylccwoheaitkqbvriqk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bGNjd29oZWFpdGtxYnZyaXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwMDM2OTEsImV4cCI6MjA4MDU3OTY5MX0.2UamHsAZOUYk93z60aTTy6EhXM4k8ul3-dIFzXhBFzk'; 
+const supabaseKey = 'sb_secret_uS1XUA4BYlrUWyV4oB6vxw_bCjEjabs'; 
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
