af7c1028849c218af632f7fc33872b3a0d8367bd
Abenet Erbelo <abeneterbelo@gmail.com>
2025-12-06 20:24:33 -0800

    signup

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -5,9 +5,9 @@ const bcrypt = require("bcrypt");
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await database.query("INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+  const [user] = await database.query("INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)", [name, email, hashedPassword]);
   mail(email, "Successful signup", "You have been successfully signed up to the LMS. You can now login and take your courses");
-  res.status(201).json({ message: "User created successfully" });
+  res.status(201).json({ success: true, message: "User created successfully", user });
 };
 
 module.exports = {signup};
