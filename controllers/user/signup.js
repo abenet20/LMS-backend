4ce67e0032a19c8d93eba6d7c0d8318207f4b923
Abenet Erbelo <abeneterbelo@gmail.com>
2025-12-06 22:19:40 -0800

    Update signup.js

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -1,13 +1,15 @@
 const database = require("../../controllers/data/database");
 const { mail } = require("../../controllers/sender/mail");
 const bcrypt = require("bcrypt");
+const generateToken = require("../utils/generateToken");
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const [user] = await database.query("INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+  const user = await database.query("INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)", [name, email, hashedPassword]);
   mail(email, "Successful signup", "You have been successfully signed up to the LMS. You can now login and take your courses");
-  res.status(201).json({ success: true, message: "User created successfully", user });
+ generateToken({ id: user.id });
+  res.status(201).json({ success: true, message: "User created successfully", user, token  });
 };
 
 module.exports = {signup};
